2269b92f297d7f6208ca009e76b98f5ee38258ce
KimJaea <rkddnjs58@gmail.com>
2023-11-01 14:21:43 +0900

    :sparkles: Add Prefab and Automate to Create Prefabs
    Add Prefab and Automate to Create Prefabs

--- Senabo/Assets/Scripts/ReceiptScene/ReceiptScene.cs
+++ Senabo/Assets/Scripts/ReceiptScene/ReceiptScene.cs
@@ -6,9 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class ReceiptScene : MonoBehaviour
 {
+    public int numberOfReceipt; // private
+
+    public GameObject receiptPrefab;
+    public Transform receiptContent;
+
     void Start()
     {
-        
+        CreateReceiptPrefabs();
+    }
+
+    void CreateReceiptPrefabs()
+    {
+        for (int i = 0; i < numberOfReceipt; i++)
+        {
+            Instantiate(receiptPrefab, receiptContent);
+        }
     }
 
     public void LoadMainScene()
