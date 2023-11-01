f4c2575b908b04087337be5a6a7dd561e5786284
KimJaea <rkddnjs58@gmail.com>
2023-11-01 16:09:31 +0900

    :sparkles: Add Camera Action
    Add Camera Action

---
+++ Senabo/Assets/Scripts/MainScene/CameraMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 dragOrigin;
+
+    private void Update()
+    {
+        PanCamera();
+    }
+
+    private void PanCamera()
+    {
+        // save position of mouse in world space when drag starts (first time clicked)
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        // calculate distance between drag origin and new position if it is still held down
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+
+            // move the camera by that distance
+            cam.transform.position += difference;
+        }
+    }
+}
