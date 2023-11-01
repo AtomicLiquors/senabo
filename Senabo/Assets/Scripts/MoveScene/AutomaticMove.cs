b9d7b5bb8b3c59d10fd4d0a3bd1702957b4d8a1a
KimJaea <rkddnjs58@gmail.com>
2023-11-01 13:11:03 +0900

    :sparkles: AutoSwitch to ReceiptScene
    AutoSwitch to ReceiptScene

--- Senabo/Assets/Scripts/MoveScene/AutomaticMove.cs
+++ Senabo/Assets/Scripts/MoveScene/AutomaticMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AutomaticMove : MonoBehaviour
 {
@@ -10,22 +11,43 @@ public class AutomaticMove : MonoBehaviour
 
     private float journeyLength;
     private float startTime;
+    private int repeatCount = 0;
     void Start()
     {
         journeyLength = Vector3.Distance(startPoint.position, endPoint.position);
         startTime = Time.time;
+        Invoke("SwitchScene", 3.0f);
     }
 
     void Update()
     {
         float distanceCovered = (Time.time - startTime) * movementSpeed;
 
-        if(distanceCovered < journeyLength) {
+        if (distanceCovered < journeyLength)
+        {
             float fractionOfJourney = distanceCovered / journeyLength;
-            transform.position=Vector3.Lerp(startPoint.position, endPoint.position, fractionOfJourney);
-        } else {
+            transform.position = Vector3.Lerp(startPoint.position, endPoint.position, fractionOfJourney);
+        }
+        else
+        {
             transform.position = startPoint.position;
             startTime = Time.time;
+            repeatCount++;
+
+            if (repeatCount == 3)
+            {
+                LoadReceiptScene();
+            }
         }
+    }
+
+    public void LoadReceiptScene()
+    {
+        SceneManager.LoadScene("ReceiptScene");
+    }
+
+    private void SwitchScene()
+    {
+        SceneManager.LoadScene("ReceiptScene");
     }
 }
