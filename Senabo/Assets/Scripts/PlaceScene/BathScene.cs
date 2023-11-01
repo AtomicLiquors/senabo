1effe62f730cf52667725e2b67fc83e06fcc4968
KimJaea <rkddnjs58@gmail.com>
2023-11-01 17:27:35 +0900

    :sparkles: Add Water Image
    Add Water Image

--- Senabo/Assets/Scripts/PlaceScene/BathScene.cs
+++ Senabo/Assets/Scripts/PlaceScene/BathScene.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class BathScene : MonoBehaviour
 {
+    public int bathState = 0;
+    public Image background;
+    public Sprite wetImage;
+
     void Start()
     {
-        
+        Invoke("ChangeImage", 3.0f);
+    }
+    
+    public void ChangeImage()
+    {
+        background.sprite = wetImage;
     }
 
     public void LoadMainScene()
