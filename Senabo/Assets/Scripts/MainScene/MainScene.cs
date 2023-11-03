94c9e9d9dc22026e65e6e2ba5a79710ad3721f46
KimJaea <rkddnjs58@gmail.com>
2023-11-03 15:54:41 +0900

    :sparkles: Add code to change title text
    Add code to change title text. This get text data from  server and show it as title bar.

--- Senabo/Assets/Scripts/MainScene/MainScene.cs
+++ Senabo/Assets/Scripts/MainScene/MainScene.cs
@@ -2,16 +2,54 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class MainSceneClass
+{
+    public long id;
+    public string name;
+    public int days;
+}
+
 public class MainScene : MonoBehaviour
 {
+    public Text MainTitleText;
+
     public GameObject actionModal;
 
     void Start()
     {
+        StartCoroutine(WebRequestGET());
+    }
 
+    IEnumerator WebRequestGET()
+    {
+        string email = "kim@ssafy.com";
+        string url = ServerSettings.SERVER_URL + "/api/member/get/" + email;
+
+        UnityWebRequest www = UnityWebRequest.Get(url);
+
+        yield return www.SendWebRequest();
+
+        if (www.error == null)
+        {
+            Debug.Log(www.downloadHandler.text); //
+            string jsonString = www.downloadHandler.text;
+            var response = JsonUtility.FromJson<APIResponse<MainSceneClass>>(jsonString);
+
+            Debug.Log("Received Object: " + response.data); //
+        }
+        else
+        {
+            MainSceneClass receivedData = new MainSceneClass { id = 123456, name = "만두", days = 30 };
+
+            MainTitleText.text = receivedData.name + "와(과) 함께한지 " + receivedData.days + "일째";
+
+            Debug.Log("WebRequest Error Occured");
+        }
     }
 
     public void LoadBathScene()
