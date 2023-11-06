adc262d3e156f6ecc7e9ae54476696fa505e8dfc
KimJaea <rkddnjs58@gmail.com>
2023-11-06 23:40:52 +0900

    :sparkles: Add API Code
    Add API Code to MealScene. Now it can get status Feed posibility.

--- Senabo/Assets/Scripts/MealScene/MealScene.cs
+++ Senabo/Assets/Scripts/MealScene/MealScene.cs
@@ -5,18 +5,65 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
 
+[System.Serializable]
+public class MealSceneClass
+{
+    public bool possibleYn;
+    public string lastFeedDateTime;
+    public string nowDateTime;
+}
+
 public class MealScene : MonoBehaviour
 {
     public Image mealImage;
     // public Sprite mealSprite;
     private int clickedCount = 0, mealState = 0, changeLimit = 1;
+    private Button button;
     void Start()
     {
-        Button button = mealImage.GetComponent<Button>();
-        button.onClick.AddListener(OnClickMeal);
+        StartCoroutine(CheckFeed());
     }
 
-    IEnumerator Upload()
+    IEnumerator CheckFeed()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("email", PlayerPrefs.GetString("email"));
+
+        // string url = ServerSettings.SERVER_URL + "/api/feed/check";
+        string url = ServerSettings.SERVER_URL + "/api/feed/check?email=" + PlayerPrefs.GetString("email"); // TEST CODE
+        UnityWebRequest request = new UnityWebRequest(url, "GET");
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log("MealScene CheckFeed Error! " + request.error);
+        }
+        else
+        {
+            Debug.Log("MealScene CheckFeed Success!");
+
+            string jsonString = request.downloadHandler.text;
+            var response = JsonUtility.FromJson<APIResponse<MealSceneClass>>(jsonString);
+
+            if (response.data.possibleYn)
+            {
+                Debug.Log("배식 가능 상태"); // Debug Code
+                button = mealImage.GetComponent<Button>();
+                button.onClick.AddListener(OnClickMeal);
+            }
+            else
+            {
+                Debug.Log("배식 불가 상태"); // Debug Code
+                mealImage.color = new Color32(0, 30, 225, 100);
+                mealState = 1;
+            }
+        }
+    }
+
+    IEnumerator GiveFeed()
     {
         WWWForm form = new WWWForm();
         form.AddField("email", PlayerPrefs.GetString("email"));
@@ -31,11 +78,11 @@ public class MealScene : MonoBehaviour
 
         if (request.isNetworkError || request.isHttpError)
         {
-            Debug.Log("MealScene Error! " + request.error);
+            Debug.Log("MealScene GiveFeed Error! " + request.error);
         }
         else
         {
-            Debug.Log("MealScene Success!");
+            Debug.Log("MealScene GiveFeed Success!");
         }
     }
 
@@ -47,7 +94,8 @@ public class MealScene : MonoBehaviour
             mealImage.color = new Color32(0, 30, 225, 100);
             mealState = 1;
 
-            StartCoroutine(Upload());
+            StartCoroutine(GiveFeed());
+            button.onClick.RemoveListener(OnClickMeal);
         }
     }
 
