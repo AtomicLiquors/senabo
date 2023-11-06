c59ac9831813e6324e54d4ff3ccf5e5783395bcb
AtomicLiquors <gyqls234@gmail.com>
2023-11-06 13:20:56 +0900

    :sparkles: 목줄 재생성

--- Senabo-AR/Assets/Scripts/DogWalkingScene/DogAnimationManager.cs
+++ Senabo-AR/Assets/Scripts/DogWalkingScene/DogAnimationManager.cs
@@ -6,22 +6,40 @@ public class DogAnimationManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject dogObject;
+    
+    [SerializeField]
+    private GameObject strapObject;
+
 
     Animator welshAnim;
+    Animator strapAnim;
     // Start is called before the first frame update
     void Start()
     {
         if (welshAnim == null)
             welshAnim = dogObject.GetComponentInChildren<Animator>();
+        if (strapAnim == null && strapObject.activeInHierarchy)
+            strapAnim = strapObject.GetComponentInChildren<Animator>();
     }
 
     public void handleDogMovement(string motion)
     {
-        if(!EventStatusManager.GetDogEvent()) welshAnim.SetTrigger(motion);
+        if (!EventStatusManager.GetDogEvent())
+        {
+            welshAnim.SetTrigger(motion);
+            if (strapObject.activeInHierarchy)
+            {
+                strapAnim.SetTrigger(motion);
+            }
+        }
     }
 
     public void handleDogSuddenEvent(string motion)
     {
         welshAnim.SetTrigger(motion);
+        if (strapObject.activeInHierarchy)
+        {
+            strapAnim.SetTrigger(motion);
+        }
     }
 }
