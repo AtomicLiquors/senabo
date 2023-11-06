62ba3229bb317c39a8dae34a9bb737f01497dd12
KimJaea <rkddnjs58@gmail.com>
2023-11-06 18:28:26 +0900

    :sparkles: Add API Code to MealScene
    Add API Code to MealScene

--- Senabo/Assets/Scripts/MealScene/MealScene.cs
+++ Senabo/Assets/Scripts/MealScene/MealScene.cs
@@ -1,32 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 public class MealScene : MonoBehaviour
 {
+    public Image mealImage;
+    // public Sprite mealSprite;
+    private int clickedCount = 0, mealState = 0, changeLimit = 1;
     void Start()
     {
-        StartCoroutine(Upload());
+        Button button = mealImage.GetComponent<Button>();
+        button.onClick.AddListener(OnClickMeal);
     }
 
     IEnumerator Upload()
     {
         WWWForm form = new WWWForm();
-        form.AddField("파라메타", "데이터");
+        form.AddField("email", PlayerPrefs.GetString("email"));
 
-        UnityWebRequest www = UnityWebRequest.Post("http://www.my-server.com/myform", form);
-        www.SetRequestHeader("헤더", "헤더 값");
-        yield return www.SendWebRequest();
+        // string url = ServerSettings.SERVER_URL + "/api/feed/save";
+        string url = ServerSettings.SERVER_URL + "/api/feed/save?email=" + PlayerPrefs.GetString("email"); // TEST CODE
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
 
-        if (www.isNetworkError || www.isHttpError)
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
         {
-            Debug.Log(www.error);
+            Debug.Log("MealScene Error! " + request.error);
         }
         else
         {
-            Debug.Log("성공!");
+            Debug.Log("MealScene Success!");
+        }
+    }
+
+    void OnClickMeal()
+    {
+        clickedCount++;
+        if (mealState == 0 && clickedCount > changeLimit)
+        {
+            mealImage.color = new Color32(0, 30, 225, 100);
+            mealState = 1;
+
+            StartCoroutine(Upload());
         }
     }
 
