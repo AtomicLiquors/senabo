d4d1c5d78201e8423589a7113eaa54195bcfa2db
KimJaea <rkddnjs58@gmail.com>
2023-11-07 14:01:51 +0900

    :sparkles: Add Buttons to HeartScene
    Add Buttons to HeartScene. Now I can select HeartType by buttons.

--- Senabo/Assets/Scripts/HeartScene/HeartScene.cs
+++ Senabo/Assets/Scripts/HeartScene/HeartScene.cs
@@ -18,32 +18,55 @@ public class HeartType
 public class HeartScene : MonoBehaviour
 {
     public Image dogImage;
-    public GameObject heartImage;
-    public int heartType; // TEST CODE
+    public GameObject heartImage, alertPanel;
+    public Button[] heartButtons;
+    private int heartType = 0;
     private Button dogBody;
-    private int heartCount = 0, heartLimit = 3;
+    private int[] heartCount = new int[5];
+    private int heartLimit = 3;
     private bool heartUploading = false;
 
     void Start()
     {
         dogBody = dogImage.GetComponent<Button>();
-        dogBody.onClick.AddListener(giveHeart);
+        dogBody.onClick.AddListener(GiveHeart);
     }
 
-    void giveHeart()
+    public void OffAlertPanel()
     {
-        if (!heartUploading)
-        {
-            Debug.Log(heartCount + "회 교감 진행"); // Debug Code
-            heartUploading = true;
-            heartCount++;
-            heartImage.SetActive(true);
-            StartCoroutine(UploadAfterDelay(1.0f));
-        }
+        alertPanel.SetActive(false);
+    }
 
-        if (heartCount > heartLimit)
+    public void SetHeartType(int type)
+    {
+        heartType = type;
+        OffAlertPanel();
+    }
+
+    void GiveHeart()
+    {
+        if (heartType == 0)
         {
-            dogBody.onClick.RemoveListener(giveHeart);
+            alertPanel.SetActive(true);
+            Invoke(nameof(OffAlertPanel), 2.0f);
+        }
+        else
+        {
+            int current = heartType / 100 - 1;
+            if (heartButtons[current].interactable && !heartUploading)
+            {
+                heartCount[current]++;
+                heartUploading = true;
+                heartImage.SetActive(true);
+                StartCoroutine(UploadAfterDelay(1.0f));
+
+                Debug.Log(GetHeartType() + " 교감을 " + heartCount[current] + "회 진행"); // Debug Code
+
+                if (heartCount[current] >= heartLimit)
+                {
+                    heartButtons[current].interactable = false;
+                }
+            }
         }
     }
 
@@ -56,7 +79,7 @@ public class HeartScene : MonoBehaviour
         heartImage.SetActive(false);
     }
 
-    string getHeartType()
+    string GetHeartType()
     {
         switch (heartType)
         {
@@ -81,10 +104,10 @@ public class HeartScene : MonoBehaviour
         WWWForm form = new WWWForm();
         form.AddField("email", PlayerPrefs.GetString("email"));
 
-        // string url = ServerSettings.SERVER_URL + "/api/communication/save/" + getHeartType();
-        string url = ServerSettings.SERVER_URL + "/api/communication/save/" + getHeartType() + "?email=" + PlayerPrefs.GetString("email"); // TEST CODE
+        // string url = ServerSettings.SERVER_URL + "/api/communication/save/" + GetHeartType();
+        string url = ServerSettings.SERVER_URL + "/api/communication/save/" + GetHeartType() + "?email=" + PlayerPrefs.GetString("email"); // TEST CODE
         UnityWebRequest request = new UnityWebRequest(url, "POST");
-        
+
         request.downloadHandler = new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
