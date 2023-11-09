3615ac581055f1761e7433836c0ae7358c17d536
KimJaea <rkddnjs58@gmail.com>
2023-11-09 12:58:54 +0900

    :sparkles: 회원가입 부분의 기능 추가

--- Senabo-AR/Assets/Scripts/SignUpProfileScene/SignUp.cs
+++ Senabo-AR/Assets/Scripts/SignUpProfileScene/SignUp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,18 +10,51 @@ public class SignUp : MonoBehaviour
 {
     public Text EmailText;
     public GameObject FailSignUpAlertModalPanel;
+    public GameObject TextLengthZeroModalPanel;
+    public GameObject TextLengthOverModalPanel;
     public InputField inputField;
 
     void Awake()
     {
         FailSignUpAlertModalPanel.SetActive(false);
+        TextLengthZeroModalPanel.SetActive(false);
+        TextLengthOverModalPanel.SetActive(false);
+
         EmailText.text = SignUpManager.signUpRequestDto.email;
+        // EmailText.text = "test@ssafy.com"; // TEST CODE
+
+        inputField.onValueChanged.AddListener(delegate { CheckInput(); });
+    }
+
+    void InvokeCloseModal()
+    {
+        FailSignUpAlertModalPanel.SetActive(false);
+        TextLengthZeroModalPanel.SetActive(false);
+        TextLengthOverModalPanel.SetActive(false);
+    }
+
+    void CheckInput()
+    {
+        if (inputField.text.Length >= 6)
+        {
+            TextLengthOverModalPanel.SetActive(true);
+            Invoke(nameof(InvokeCloseModal), 2.0f);
+            inputField.text = inputField.text[..5];
+        }
     }
 
     public void OnAdoptButtonClick()
     {
-        SignUpManager.signUpRequestDto.dogName = inputField.text;
-        StartCoroutine(PostMember());
+        if (inputField.text.Length == 0)
+        {
+            TextLengthZeroModalPanel.SetActive(true);
+            Invoke(nameof(InvokeCloseModal), 1.0f);
+        }
+        else
+        {
+            SignUpManager.signUpRequestDto.dogName = inputField.text;
+            StartCoroutine(PostMember());
+        }
     }
 
     void CloseFailSignUpAlertModalPanel()
@@ -46,15 +80,15 @@ public class SignUp : MonoBehaviour
 
             if (request.error == null)
             {
-                Debug.Log(request.downloadHandler.text);
+                Debug.Log(request.downloadHandler.text); // Debug Code
                 SignUpResponseDtoClass signUpResponseDto = JsonUtility.FromJson<APIResponse<SignUpResponseDtoClass>>(request.downloadHandler.text).data;
-                PlayerPrefs.SetString("email", signUpResponseDto.email); 
+                PlayerPrefs.SetString("email", signUpResponseDto.email);
                 PlayerPrefs.SetString("dogName", signUpResponseDto.dogName);
                 SceneManager.LoadScene("MainScene");
             }
             else
             {
-                Debug.Log("회원가입 실패");
+                Debug.Log("회원가입 실패"); // Debug Code
                 FailSignUpAlertModalPanel.SetActive(true);
                 Invoke("CloseFailSignUpAlertModalPanel", 2.0f);
             }
