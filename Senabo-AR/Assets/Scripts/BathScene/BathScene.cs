3d592c40ed2215144039c84193c2b4de1fe632d4
KimJaea <rkddnjs58@gmail.com>
2023-11-10 14:10:56 +0900

    :sparkles: Add Function to BathScene
    Add function to select between bath and brush, but will never use.

--- Senabo-AR/Assets/Scripts/BathScene/BathScene.cs
+++ Senabo-AR/Assets/Scripts/BathScene/BathScene.cs
@@ -7,17 +7,62 @@ using UnityEngine.Networking;
 
 public class BathScene : MonoBehaviour
 {
-    public Image background, dogImage;
-    public Sprite wetImage;
-    private int clickedCount = 0, bathState = 0, changeLimit = 10;
+    public Text titleBarText;
+    public GameObject titleBar, dogBodyImage, dogFaceImage;
+    public Image background;
+    // public Sprite wetImage; // for background
+    private int selectType; // 1: Bath, 2: Teeth
+    private Button dogBody, dogFace;
 
     void Start()
     {
-        Button button = dogImage.GetComponent<Button>();
-        button.onClick.AddListener(OnClickDog);
+        setSelectType(1);
+    }
+    
+    public void setSelectType(int type)
+    {
+        selectType = type;
+
+        if (type == 1)
+        {
+            titleBarText.text = "목욕";
+            dogBodyImage.SetActive(true);
+            dogBody = dogBodyImage.GetComponent<Button>();
+            dogBody.onClick.AddListener(OnClickDogBody);
+        }
+        else
+        {
+            titleBarText.text = "양치";
+            dogFaceImage.SetActive(true);
+            dogFace = dogFaceImage.GetComponent<Button>();
+            dogFace.onClick.AddListener(OnClickDogFace);
+        }
+        titleBar.SetActive(true);
     }
 
-    IEnumerator Upload()
+    void OnClickDogBody()
+    {
+        // 목욕
+
+        // A 횟수 동안 거품내는 모션
+        // 강아지 주변에 거품 이미지 생성
+
+        // B 횟수 동안 물로 씻는 모션
+        // 샤워기로 물 뿌림 + 거품 소멸
+        // 욕실 바닥에 물 생성
+
+        // 다 씻은 후 강아지 주변에 반짝 거림
+        // 다 씼었음 알리는 Alert 등장
+
+        StartCoroutine(DoBath());
+    }
+
+    void OnClickDogFace()
+    {
+        StartCoroutine(BrushTeeth());
+    }
+
+    IEnumerator DoBath()
     {
         WWWForm form = new WWWForm();
         form.AddField("email", PlayerPrefs.GetString("email"));
@@ -32,22 +77,34 @@ public class BathScene : MonoBehaviour
 
         if (request.isNetworkError || request.isHttpError)
         {
-            Debug.Log("BathScene Error! " + request.error);
+            Debug.Log("BathroomScene DoBath Error! " + request.error);
         }
         else
         {
-            Debug.Log("BathScene Success!");
+            Debug.Log("BathroomScene DoBath Success!");
         }
     }
 
-    void OnClickDog()
+    IEnumerator BrushTeeth()
     {
-        clickedCount++;
-        if(bathState == 0 && clickedCount > changeLimit) {
-            background.sprite = wetImage;
-            bathState = 1;
-            
-            StartCoroutine(Upload());
+        WWWForm form = new WWWForm();
+        form.AddField("email", PlayerPrefs.GetString("email"));
+
+        // string url = ServerSettings.SERVER_URL + "/api/brushing-teeth/save";
+        string url = ServerSettings.SERVER_URL + "/api/brushing-teeth/save?email=" + PlayerPrefs.GetString("email"); // TEST CODE
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log("BathroomScene BrushTeeth Error! " + request.error);
+        }
+        else
+        {
+            Debug.Log("BathroomScene BrushTeeth Success!");
         }
     }
 
