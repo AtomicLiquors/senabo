9334719c48876abf51cafd9248ddb4d9bb8152f6
AtomicLiquors <gyqls234@gmail.com>
2023-11-12 16:35:11 +0900

    :bug: welsh active fixed

---
+++ Senabo-AR/Assets/Scripts/DogWalkingScene/DogOffscreenIndicator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogOffscreenIndicator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject dogObject;
+
+    [SerializeField]
+    GameObject dogIndicator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
